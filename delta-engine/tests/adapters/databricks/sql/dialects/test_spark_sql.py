e91ec5ed03f6276eb3b8d8a0d481a780104d2c93
Tomos Corbin <tom_corbin1@hotmail.com>
2025-08-26 19:42:29 +0100

    dialects tests removed

--- delta-engine/tests/adapters/databricks/sql/dialects/test_spark_sql.py
+++
@@ -1,6 +0,0 @@
-from delta_engine.adapters.databricks.sql.dialects import SqlDialect
-from delta_engine.adapters.databricks.sql.dialects.spark_sql import SPARK_SQL
-
-
-def test_spark_sql_conforms_to_protocol() -> None:
-    assert isinstance(SPARK_SQL, SqlDialect)
